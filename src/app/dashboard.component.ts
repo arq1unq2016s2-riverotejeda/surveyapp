5b8f9797b8181bc8813ac7cbdc987e74324ae5c1
Mar <tejedamarcia@gmail.com>
2016-11-08 07:47:29 -0300

    testing lists and adding bootstrap

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -3,13 +3,39 @@
  */
 
 
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subject} from "./model/subject";
+import {Observable} from "rxjs";
+import {SubjectService} from "./services/subject.service";
+import {SurveyService} from "./services/survey.service";
 
 
 @Component({
   selector: 'my-home',
-  templateUrl: './templates/dashboard_template.html'
+  templateUrl: './templates/dashboard_template.html',
+  providers: [SubjectService, SurveyService]
 })
 
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit{
+
+  subjects: Subject[];
+  constructor(private subjectService: SubjectService) {
+
+  }
+
+  // Load data ones componet is ready
+  ngOnInit() {
+   this.getSubjects();
+  }
+
+  getSubjects() {
+    this.subjectService.getSubjects()
+      .subscribe(
+        res =>
+          this.subjects = res,
+        error => console.log("Error HTTP GET Service") // in case of failure show this message
+      );
+  }
+
+}
 
