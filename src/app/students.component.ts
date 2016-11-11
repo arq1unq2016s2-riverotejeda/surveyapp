74f87525b50b0dbc39121a9eb8a16a58ab880868
Marcia Tejeda <tejedamarcia@gmail.com>
2016-11-11 12:22:01 -0300

    saving students

--- src/app/students.component.ts
+++ src/app/students.component.ts
@@ -1,3 +1,4 @@
+
 /**
  * Created by mar on 06/11/16.
  */
@@ -7,26 +8,33 @@
 import { Component } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {Student} from "./model/student";
+import {StudentService} from "./services/student.service";
 
 
 @Component({
   selector: 'dashboard-elem',
-  templateUrl: 'templates/students_template.html'
+  templateUrl: 'templates/students_template.html',
+  providers: [StudentService]
 })
 
 export class StudentsComponent {
 
   public model = new Student("", "");
-  constructor(private route: ActivatedRoute, private router: Router){
+  active = true;
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private studentService: StudentService){}
 
-
+  onSubmit() {
+    this.studentService.saveStudent(this.model).subscribe(
+      response => console.log(response),
+      () => console.log('Student successfully saved')
+    );
+    this.goToThanksPage();
   }
-
-
 
 
   goToThanksPage(){
     this.router.navigate(['/message', {mes: 'Alumno guardado correctamente'}] );
-
   }
 }
