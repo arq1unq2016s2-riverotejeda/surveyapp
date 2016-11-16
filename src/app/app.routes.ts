3b075569af397409788d8bc09fe5679f848b16bb
Marcia Tejeda <tejedamarcia@gmail.com>
2016-11-16 16:13:10 -0300

    heroku

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ import {OverviewComponent} from "./overview.component";
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'message', component: MessageComponent },
+  { path: 'hola', component: DashboardComponent},
   { path: 'dashboard', component: DashboardComponent,
     children: [
       { path: '', redirectTo: 'overview', pathMatch: 'full' },
