180701ba2dd38c967dcbb0167829b2b5906c6f74
mriverov <mriverov3@gmail.com>
2016-11-18 00:04:18 -0300

    prueba heroku angular-cli

--- src/app/home.constants.ts
+++ src/app/home.constants.ts
@@ -8,5 +8,5 @@ import {environment} from "../environments/environment";
 @Injectable()
 export class Configuration {
     public static SERVER: string = environment.surveyBackendHost;
-    public static API_ENDPOINT: string = Configuration.SERVER;
+    public static API_ENDPOINT: string = "https://survey-backoffice.herokuapp.com";
 }
