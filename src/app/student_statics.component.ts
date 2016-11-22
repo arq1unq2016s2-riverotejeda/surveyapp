874bd47913b28b97d35d5c8cff8ed3fc6acbee2d
Marcia Tejeda <tejedamarcia@gmail.com>
2016-11-22 17:43:59 -0300

    classes

--- src/app/student_statics.component.ts
+++ src/app/student_statics.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import Any = jasmine.Any;
 import {StaticsService} from "./services/statics.service";
+import {Subject} from "./model/subject";
 
 @Component({
   selector: 'my-home',
@@ -9,11 +10,11 @@ import {StaticsService} from "./services/statics.service";
   providers: [StaticsService]
 })
 export class StudentStaticsComponent implements OnInit{
-
+  status ="success";
   subjectsStatistics: Any[];
   constructor(private staticsService: StaticsService,
               private router: Router){
-    status ="success";
+
   }
 
   ngOnInit() :void{
@@ -25,9 +26,23 @@ export class StudentStaticsComponent implements OnInit{
       .subscribe(
         res => {
           this.subjectsStatistics = res;
+          console.log(this.subjectsStatistics);
+
         },
         error => console.log("Error HTTP GET Service") // in case of failure show this message
       );
   }
+
+  getStatus(subject: Subject): String{
+
+}
+/*
+  getClasses() {
+    return {
+      'panel': true,
+      'panel-default': !this.seleccion,
+      'panel-success': this.seleccion
+    };
+  }*/
 
 }
