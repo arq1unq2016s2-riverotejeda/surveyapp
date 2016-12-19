276b7cef850afeeb51e945c1445db9213ac0cc23
Mar <tejedamarcia@gmail.com>
2016-12-19 01:11:15 -0300

    lookandfeel table

--- src/app/model/subject_statistic.ts
+++ src/app/model/subject_statistic.ts
@@ -7,12 +7,12 @@ export class SubjectStatistic {
   status: string;
 
   constructor(public subject: string,
-              public comision: string,
-              public occupation: number,
-              public percentage: number){
+    public comision: string,
+    public occupation: number,
+    public percentage: number) {
 
-          if (percentage > 90) { this.status = "warning"}
-              else {this.status = "success"};
+    if (percentage > 90) { this.status = "warning" }
+    else { this.status = "success" };
   }
 
 
@@ -21,3 +21,32 @@ export class SubjectStatistic {
 
 
 
+export class Statistic {
+
+
+  constructor(public subject: string,
+    public not_yet: number,
+    public approved: number,
+    public bad_schedule: number,
+    public c1:ComisionData,
+    public c2:ComisionData,
+    public c3:ComisionData,
+    public c4:ComisionData) {
+  }
+}
+
+export class ComisionData {
+
+  status: string;
+  constructor(public comision: string, public occupation: any, public quota: any) {
+    status = "success";
+    if ((quota - occupation) < 3) { this.status = "warning" }
+    else { this.status = "success" };
+    if (quota == 0){
+      this.occupation= "-";
+      this.quota= "-";
+    }
+  //  console.log(comision+'  '+status);
+  }
+
+}
--- src/app/student_statics.component.ts
+++ src/app/student_statics.component.ts
@@ -2,8 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {Router, ActivatedRoute} from "@angular/router";
 import Any = jasmine.Any;
 import {StaticsService} from "./services/statics.service";
-import {SubjectStatistic} from "./model/subject_statistic";
 import {SubjectService} from "./services/subject.service";
+import {SubjectStatistic, ComisionData, Statistic} from "./model/subject_statistic";
+
 
 @Component({
   selector: 'my-home',
@@ -11,8 +12,10 @@ import {SubjectService} from "./services/subject.service";
   providers: [StaticsService, SubjectService]
 })
 export class StudentStaticsComponent implements OnInit{
-  subjectsStatistics: SubjectStatistic[];
+  public statistics: Statistic[];
   private year: string;
+
+  subjectsStatisticsBySubject: any;
 
 
   constructor(private staticsService: StaticsService,
@@ -26,7 +29,6 @@ export class StudentStaticsComponent implements OnInit{
         if(params['year']){
           this.year = params['year'];
           this.getStatistics();
-
         }else{
           this.subjectService.getLastActiveYear().subscribe(
             res => {
@@ -42,26 +44,75 @@ export class StudentStaticsComponent implements OnInit{
     this.staticsService.getSubjectsStatistics(this.year)
       .subscribe(
         res => {
-          //this.subjectsStatistics = res;
-          //console.log(res);
-          this.subjectsStatistics  = this.makeJson(res);
-          //console.log(this.subjectsStatistics);
+          this.statistics  = this.makeJson(res);
         },
         error => console.log("Error HTTP GET Service") // in case of failure show this message
       );
   }
 
 
-  makeJson(se: SubjectStatistic[]): SubjectStatistic[]{
-    let res: SubjectStatistic[] = [];
+  makeJson(se: Statistic[]): Statistic[]{
+    let res: Statistic[] = [];
     for (let index in se){
-      var sub =    se[index];
-      var stat = new SubjectStatistic(sub.subject, sub.comision, sub.occupation, sub.percentage);
+      var sub =  se[index];
+      var c1 = new ComisionData("C1", sub.c1.occupation, sub.c1.quota);
+      var c2 = new ComisionData("C2", sub.c2.occupation, sub.c2.quota);
+      var c3 = new ComisionData("C3", sub.c3.occupation, sub.c3.quota);
+      var c4 = new ComisionData("C4", sub.c4.occupation, sub.c4.quota);
+      var stat = new Statistic(sub.subject, sub.not_yet, sub.approved, sub.bad_schedule, c1, c2, c3, c4);
       res[index] = stat;
      }
      return res;
-
   }
+
+
+
+  /*groupStatistics(){
+    this.subjectsStatisticsBySubject = [];
+    var statistics = [];
+    for(var index in this.subjectsStatistics){
+      var currentSubject = this.subjectsStatistics[index];
+      statistics[currentSubject.subject] = [];
+    }
+
+    for(var index in this.subjectsStatistics){
+      var currentSubject = this.subjectsStatistics[index];
+      statistics[currentSubject.subject].push(currentSubject);
+    }
+
+    var statisticsBySubject = [];
+    for(var index in statistics){
+      var listC = [];
+      for(var i in statistics[index]){
+        var currentStatistic = statistics[index][i];
+        var com = new ComisionData(currentStatistic.comision, currentStatistic.percentage, currentStatistic.occupation);
+        listC.push({clave: currentStatistic.comision, valor: com})
+      }
+      statisticsBySubject.push({clave : index, valor :listC});
+    }
+
+    for(var index in statisticsBySubject){
+      var current = statisticsBySubject[index];
+        var s = new Statistic(current.clave,  0, 0, 0);
+        for(var i in current.valor){
+          var c = current.valor[i];
+          if (c.clave == "C1") {
+            s.c1 = c.valor;
+          }
+          if (c.clave == "C2") {
+            s.c2 = c.valor;
+          }
+          if (c.clave == "C3") {
+            s.c3 = c.valor;
+          }
+          if (c.clave == "C4") {
+            s.c4 = c.valor;
+          }
+        }
+        this.subjectsStatisticsBySubject.push(s);
+    }
+  }*/
+
 
 
 }
